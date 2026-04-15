ccbbb43f58cbafd983c3206f82bc3231a4408d0e
iyern5136 <iyern5136@my.sduhsd.net>
2026-04-15 01:51:16 +0000

    spaghetti

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // add javascript here
+var i = 0;
+var txt = 'Volleyball is a fun sport. It is played by both men and women, and is offered in most high schools. I am Nina Iyer, a 9th grader at Canyon Crest Academy, and I have played both indoor and beach volleyball here at CCA. This site is a guide to the world of volleyball, based on my experience!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    console.log("button clicked");
+  if (i < txt.length) {
+    document.getElementById("aboutme").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
