cd2c4337043316d4c56a167649353ec87050e81d
iyern5136 <iyern5136@my.sduhsd.net>
2026-04-15 02:19:29 +0000

    april 14

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// add javascript here
+// typewriter effect
+
 var i = 0;
 var txt = 'Volleyball is a fun sport. It is played by both men and women, and is offered in most high schools. I am Nina Iyer, a 9th grader at Canyon Crest Academy, and I have played both indoor and beach volleyball here at CCA. This site is a guide to the world of volleyball, based on my experience!'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
@@ -10,4 +11,38 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
+}
+
+
+
+//back to top button
+
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+//light mode button
+function lightmode() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
 }
